b33b7a9cf7cfc407afeb5c21ad27dc74d87692dc
hiroto <o.hyshrt.19@gmail.com>
2022-03-25 19:50:03 +0700

    update

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Author;
+use Illuminate\Support\Facades\Auth;//追記
 
 class AuthorController extends Controller
 {
@@ -17,10 +18,17 @@ class AuthorController extends Controller
     //     $items = Author::simplePaginate(4);
     //     return view('index', ['items' => $items]);
     // }
+    // public function index()
+    // {
+    //     $items = Author::Paginate(4);
+    //     return view('index', ['items' => $items]);
+    // }
     public function index()
     {
-        $items = Author::Paginate(4);
-        return view('index', ['items' => $items]);
+        $user = Auth::user();
+        $items = Author::paginate(4);
+        $param = ['items' => $items, 'user' =>$user];
+        return view('index', $param);
     }
     public function find()
     {
@@ -83,4 +91,24 @@ class AuthorController extends Controller
         $param = ['hasItems' => $hasItems, 'noItems' => $noItems];
         return view('author.index',$param);
     }
+    
+    public function check(Request $request)
+    {
+    $text = ['text' => 'ログインして下さい。'];
+    return view('auth', $text);
+    }
+
+    public function checkUser(Request $request)
+    {
+    $email = $request->email;
+    $password = $request->password;
+    if (Auth::attempt(['email' => $email,
+            'password' => $password])) {
+        $text =   Auth::user()->name . 'さんがログインしました';
+    } else {
+        $text = 'ログインに失敗しました';
+    }
+    return view('auth', ['text' => $text]);
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\SessionController;
+use App\Models\Person;
 
-Route::get('/home', [AuthorController::class, 'index']);
+// Route::get('/home', [AuthorController::class, 'index']);
+Route::get('/home', [AuthorController::class, 'index'])->middleware('auth');
+Route::get('/auth', [AuthorController::class,'check']);
+Route::post('/auth', [AuthorController::class,'checkUser']);
 Route::get('/add', [AuthorController::class, 'add']);
 Route::post('/add', [AuthorController::class, 'create']);
 Route::get('/edit', [AuthorController::class, 'edit']);
@@ -15,11 +19,13 @@ Route::post('/delete', [AuthorController::class, 'remove']);
 Route::get('/find', [AuthorController::class, 'find']);
 Route::post('/find', [AuthorController::class, 'search']);
 Route::get('/author/{author}',[AuthorController::class, 'bind']);
+
 Route::prefix('book')->group(function () {
   Route::get('/', [BookController::class, 'index']);
   Route::get('/add', [BookController::class, 'add']);
   Route::post('/add', [BookController::class, 'create']);
 });
+
 Route::get('/relation', [AuthorController::class, 'relate']);
 
 Route::get('/session', [SessionController::class, 'getSes']);
@@ -32,5 +38,9 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::get('/softdelete', function () {
+    Person::find(1)->delete();
+});
 
 require __DIR__.'/auth.php';
